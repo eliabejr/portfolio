e808c6302a3f2e352a399b4df6adac494e78bb71
eliabejr <eliabejunior@hotmail.com.br>
2018-09-13 01:30:11 -0300

    Create theme file

---
+++ src/utils/theme.jsx
@@ -0,0 +1,14 @@
+
+const theme = {
+  fonts: {
+    title: 'Muli, sans-serif',
+    subtitle: 'Quicksand, sans-serif',
+    text: 'Slabo 27px',
+  },
+  colors: {
+    primary: '#3498db',
+    darkBg: '#252A34',
+  },
+}
+
+export default theme
