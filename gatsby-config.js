fecf056b3084b5d879d3a08886a61314c966a991
eliabejr <eliabejunior@hotmail.com.br>
2018-09-13 01:30:38 -0300

    Add some gatsby plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,35 @@
 module.exports = {
   siteMetadata: {
-    title: 'Eliabe Junior Portfolio',
+    title: 'Eliabe Junior',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`quicksand\:300,400,900`, `mulli\: 300,400,700`, `slabo 27px\: 400`],
+      },
+    },
+    'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Eliabe Jr',
+        short_name: 'eliabejr',
+        description: 'A static portfolio made with Gatsby',
+        start_url: 'http://eliabejr.com',
+        background_color: '#fff',
+        theme_color: '#3498db',
+        display: 'minimal-ui',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `#3498db`,
+        showSpinner: true,
+      },
+    },
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+  ],
 }
